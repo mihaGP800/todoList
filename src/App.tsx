be3530943010dc1ee093b37799c385708e998516
Acer <mihagoroh@mail.com>
2022-02-20 08:49:38 +0300

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         {id: v1(), title: 'CSS', isDone: true},
         {id: v1(), title: 'JS/TS', isDone: false},
     ])
-    const [filter, setFilter] = useState<FilterValuesType>("all")
+
 
     const removeTask = (taskID: string) => {
         const filteredTasks = tasks.filter(task => task.id !== taskID)
@@ -25,8 +25,11 @@ function App() {
         // const updatedTasks = [newTask, ...tasks]
         // setTasks(updatedTasks)
         // setTasks([{id: v1(), title: title, isDone: false}, ...tasks])
-        if(title!=='') {setTasks([{id: v1(), title, isDone: false}, ...tasks])}
+        if (title !== '') {
+            setTasks([{id: v1(), title, isDone: false}, ...tasks])
+        }
     }
+    const [filter, setFilter] = useState<FilterValuesType>("all")
 
     const changeFilter = (filter: FilterValuesType) => {
         setFilter(filter)
